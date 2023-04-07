8e2bfae6dbcef640ddaa6084d126b79b73982ec0
Kenny <knnyshn@gmail.com>
2023-04-07 10:48:37 -0400

    unfuck everything

--- Client/src/Components/Posts.js
+++ Client/src/Components/Posts.js
@@ -1,7 +1,8 @@
 // import React, { useState, useEffect } from 'react';
 // import axios from 'axios';
 import Sidebar from './Sidebar';
-
+// import Thumbnail from './Thumbnail';
+// import VoteButton from './VoteButton';
 
 // This is the component for the Posts on the front (or home) page of Reddit.
 
--- Client/src/index.js
+++ Client/src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import '../src/Styles/global.css';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Router>
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <Router>
 
-    <App />
+//     <App />
     
-  </Router>
+//   </Router>
+// );
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+    <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
