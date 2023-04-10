7c850ac562d9672e0d59c24ac4ec03eb104f05f1
Kenny <knnyshn@gmail.com>
2023-04-10 09:31:36 -0400

    Initial linking of front and back

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './Components/Home';
+import PostDetails from './Components/PostDetails';
 
 function App() {
   return (
@@ -7,7 +8,8 @@ function App() {
       <Routes>
         
         <Route path="/" element={<Home />} />
-        
+        <Route path="/post/:postId" element={<PostDetails />} />
+
       </Routes>
     </>
   )
--- Client/src/Components/Navbar/Navbar.js
+++ Client/src/Components/Navbar/Navbar.js
@@ -7,6 +7,9 @@ function NavBar() {
       <div className="logo">
         <img src="/Rabbit.jpeg" alt="logo" />
       </div>
+      <div className='text-logo'>
+        <img src="/Assets/RABBIT.jpg" alt="" />
+      </div>
       <ul className="nav-links">
         <li><a href="Home">Home</a></li>
         <li><a href="About">About</a></li>
