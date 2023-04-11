3c9f05659a10f2c50b98a9a7a28ebdb23ba96905
Marshall <Evernight27@hotmail.com>
2023-04-11 08:02:07 -0400

    made auth

--- Client/src/Components/Posts.js
+++ Client/src/Components/Posts.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import Sidebar from './Sidebar/Sidebar';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthenticationContext } from '../App';
 // import Thumbnail from './Thumbnail';
 // import VoteButton from './VoteButton';
 
@@ -10,7 +11,19 @@ import { Link } from 'react-router-dom';
 function Posts() {
 
   const [posts, setPosts] = useState([]);
+  // let postsdummy = Array(10).fill();
+  // useEffect(() => {
+  //   fetch('https://rabbit-app-back.herokuapp.com/burrows/?format=json')
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       console.log(data.)
+  //       // setPosts(Object.values(data))
+  //     });
+  // }, []);
 
+  let { user, setUser } = useContext(AuthenticationContext)
+
+  const navigate = useNavigate()
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await axios.get('https://rabbit-app-back.herokuapp.com/posts/?format=json', {
@@ -19,42 +32,44 @@ function Posts() {
           // Authorization: `JWT ${localStorage.getItem('token')}`
         }
       });
-      console.log(response);
+      //console.log(response);
       setPosts(response.data);
     }
     fetchPosts();
   }, []);
-
+  if (!user) {
+    navigate('/auth/login/')
+  }
   return (
     <div className='beeg-container'>
       <div className='posts-container'>
         {posts.map((post) => (
-          <div className=''>
-          {/* UP/DOWNVOTE
+          <div key={post.id} className=''>
+            {/* UP/DOWNVOTE
           THUMBNAIL */}
-          <div className='post'>
-            <p className='single-post'/>
-          <Link to={`/burrow/${post.burrow.name}/${post.id}/`}>
-            <div className='title'>
-              <p>{post.title}</p>
+            <div className='post px-2'>
+              <p className='single-post' />
+              <Link to={`/post/${post.id}`}>
+                <div className='title'>
+                  <p>{post.title}</p>
+                </div>
+              </Link>
+              <div className='post-details'>
+                <p className='tagline'>
+                  submitted by {post.user} in {post.burrow}
+                </p>
+              </div>
+              <ul className='post-buttons'>
+                <button className='comments'>{post.comments.length} Comments</button>
+                <button className='share'>Share</button>
+                <button className='save'>Save</button>
+              </ul>
             </div>
-          </Link>
-            <div className='post-details'>
-              <p class='tagline'>
-                submitted by {post.user.username} in {post.burrow.name}.
-              </p>
-            </div>
-            <ul className='post-buttons'>
-              <button className='comments'>{post.comments.length} Comments</button>
-              <button className='share'>Share</button>
-              <button className='save'>Save</button>
-            </ul>
-          </div>
           </div>
         ))}
       </div>
-    <div className='posts-container'>
-      
+      <div className='posts-container'>
+
       </div>
       <Sidebar />
     </div>
--- Client/src/api/user.js
+++ Client/src/api/user.js
@@ -1,22 +1,29 @@
-import api from './apiConfig.js'
+import axios from 'axios'
 
 const LOCALSTORAGE_KEY = 'TOKEN'
 
 export async function signIn(username, password) {
-  const response = await api.post('/auth/login', {
-    username, password
-  })
-  console.log({ username });
-  console.log({ password });
-  localStorage.setItem(LOCALSTORAGE_KEY, response.data)
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }
+  const { data } = await axios.post("http://127.0.0.1:8000/api/auth/login/", { username: username.toLowerCase(), password: password }, config)
+  console.log(data);
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data))
 
-  return response
+  return data
 }
 
 export async function signUp(username, password) {
-  const response = await api.post('/auth/signup', {
-    username, password
-  })
-  localStorage.setItem(LOCALSTORAGE_KEY, response.data)
-  return response.data
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }
+  const { data } = await axios.post("http://127.0.0.1:8000/api/auth/signup/", { username: username.toLowerCase(), password: password }, config)
+  console.log(data);
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data))
+
+  return data
 }
